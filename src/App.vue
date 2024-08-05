6a1af13c7e6c4d2e8572363db133d564adc30f0a
Arsnyi <arsenyi.popov@gmail.com>
2024-08-05 17:43:46 +0300

    05.05

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,82 @@
 <script setup>
+import { ref } from 'vue';
 import Select from './components/Select.vue';
+
+const isError = ref(false);
+const errorMessage = ref("Выберите компанию из списка");
+
+// Функция валидации
+const validateFunction = (value, items) => {
+  if (!value) {
+    return {
+      isError: true,
+      errorMessage: "Поле не должно быть пустым",
+    };
+  }
+
+  const hasMatch = items.some(item => item.name.toLowerCase().includes(value.toLowerCase()));
+  return {
+    isError: !hasMatch,
+    errorMessage: !hasMatch ? "Компания не найдена в списке" : "",
+  };
+};
+
+// Пример данных
+const items = [
+  { name: 'Газпром', isError: true, errorMessage: 'Газпром недоступен для выбора', readOnly: false },
+  { name: 'Норильский никель', isError: false, errorMessage: '', readOnly: false },
+  { name: 'Татнефть', isError: false, errorMessage: '', readOnly: false },
+  { name: 'Сахалинская энергия', isError: false},
+  { name: 'Сбербанк', isError: false, errorMessage: '', readOnly: false },
+  { name: 'Фосагро', isError: false, errorMessage: '', readOnly: false },
+  { name: 'Арктикгаз', isError: false, errorMessage: '', readOnly: false },
+  { name: 'Лаборатория Касперского', isError: false, errorMessage: '', readOnly: false },
+  { name: 'Центральное КБ морской техники «Рубин»', isError: false, errorMessage: '', readOnly: false },
+];
+const itemsReadOnly = [
+  { name: 'Газпром', isError: true, errorMessage: 'Газпром недоступен для выбора', readOnly: false },
+  { name: 'Норильский никель', isError: false, errorMessage: '', readOnly: false },
+  { name: 'Татнефть', isError: false, errorMessage: '', readOnly: false },
+  { name: 'Сахалинская энергия', isError: false, errorMessage: '', readOnly: true },
+  { name: 'Сбербанк', isError: false, errorMessage: '', readOnly: false },
+  { name: 'Фосагро', isError: false, errorMessage: '', readOnly: false },
+  { name: 'Арктикгаз', isError: false, errorMessage: '', readOnly: false },
+  { name: 'Лаборатория Касперского', isError: false, errorMessage: '', readOnly: false },
+  { name: 'Центральное КБ морской техники «Рубин»', isError: false, errorMessage: '', readOnly: false },
+];
 </script>
 
 <template>
   <div>
+    <!-- Первый компонент Select с данными isError: true -->
     <h1>Select Component Example Vue 3</h1>
+    <h2>isError: true</h2>
     <Select
-      :items="['Газпром', 'Норильский никель', 'Татнефть', 'Сахалинская энергия', 'Сбербанк', 'Фосагро', 'Арктикгаз','Лаборатория Касперского','Центральное КБ морской техники «Рубин»']"
+      :items="items"
       placeholder="Placeholder"
       title="title:"
       hintIcon="ℹ️"
       hintText="Choose your company"
+      :validateFn="validateFunction"
+      :generalErrorMessage="errorMessage"
+      :generalIsError="isError"
+    />
+<!-- Второй компонент Select с данными для ReadOnly -->
+    <h1>Select Component Example Vue 3</h1>
+    <h2>readOnly:true</h2>
+    <Select
+      :items="itemsReadOnly"
+      placeholder="Placeholder"
+      title="title:"
+      hintIcon="ℹ️"
+      hintText="Choose your company"
+      :validateFn="validateFunction"
+      :generalErrorMessage="errorMessage"
+      :generalIsError="isError"
     />
   </div>
 </template>
 
 <style scoped>
-
-
-
-
-
+/* Добавьте свои стили при необходимости */
 </style>
--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,201 +1,297 @@
 <template>
-    <div class="select-container">
-      <div class="select-input-wrapper">
-        <label v-if="title" class="select-title">{{ title }}</label>
-        <div class="input-wrapper">
-          <input
-            type="text"
-            class="select-input"
-            v-model="searchQuery"
-            @focus="onFocus"
-            @blur="onBlur"
-            @input="filterItems"
-            :placeholder="currentPlaceholder"
-            ref="selectInput"
-            :title="searchQuery.length > maxInputLength ? searchQuery : ''"
-          />
-          <span v-if="hintIcon" class="hint-icon" @mouseover="showHint = true" @mouseleave="showHint = false">
-            {{ hintIcon }}
-            <span v-if="showHint" class="hint-text">{{ hintText }}</span>
-          </span>
-        </div>
+  <div class="select-container">
+    <div class="select-input-wrapper">
+      <label v-if="title" class="select-title">{{ title }}</label>
+      <div class="input-wrapper">
+        <input
+          type="text"
+          class="select-input"
+          v-model="searchQuery"
+          @focus="onFocus"
+          @blur="onBlur"
+          @input="onInput"
+          :placeholder="currentPlaceholder"
+          ref="selectInput"
+          :title="searchQuery.length > maxInputLength ? searchQuery : ''"
+          :readonly="isReadOnly"
+        />
+        <span v-if="hintIcon" class="hint-icon" @mouseover="showHint = true" @mouseleave="showHint = false">
+          {{ hintIcon }}
+          <span v-if="showHint" class="hint-text">{{ hintText }}</span>
+        </span>
       </div>
-      <span class="icon" :class="{ 'arrow-up': isOpen, 'arrow-down': !isOpen }" @click="toggleDropdown"></span>
-      <Dropdown v-if="isOpen" :items="filteredItems" @select="selectItem" />
+      <span
+        v-if="!isReadOnly"
+        class="icon"
+        :class="{ 'arrow-up': isOpen, 'arrow-down': !isOpen }"
+        @click="toggleDropdown"
+      ></span>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref, computed, nextTick } from 'vue';
-  import Dropdown from './Dropdown.vue';
-  
-  const props = defineProps({
-    items: {
-      type: Array,
-      required: true,
-    },
-    placeholder: {
-      type: String,
-      default: 'Select a fruit',
-    },
-    searchPlaceholder: {
-      type: String,
-      default: 'Search',
-    },
-    title: {
-      type: String,
-      default: '',
-    },
-    hintIcon: {
-      type: String,
-      default: '',
-    },
-    hintText: {
-      type: String,
-      default: '',
-    },
-  });
-  
-  const searchQuery = ref('');
-  const isOpen = ref(false);
-  const selectedValue = ref(null);
-  const showHint = ref(false);
-  const isItemSelected = ref(false);
-  const previousValue = ref('');
-  const currentPlaceholder = ref(props.placeholder); // Динамический плейсхолдер
-  const maxInputLength = 20; // Максимальная длина значения в поле ввода
-  
-  const filterItems = () => {
-    if (!isOpen.value) {
-      isOpen.value = true;
-    }
-  };
-  
-  const onFocus = () => {
-    previousValue.value = searchQuery.value;
-    if (isItemSelected.value) {
-      searchQuery.value = '';
-      isItemSelected.value = false;
-    }
+    <p v-if="generalErrorState" class="error-message">{{ generalErrorMessage }}</p>
+    <p v-if="companyErrorState" class="error-message">{{ companyErrorMessage }}</p>
+    <Dropdown v-if="isOpen && !isReadOnly" :items="filteredItems" @select="selectItem" />
+  </div>
+</template>
+
+<script setup>
+import { ref, computed, nextTick, watch } from 'vue';
+import Dropdown from './Dropdown.vue';
+
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+  placeholder: {
+    type: String,
+    default: 'Placeholder',
+  },
+  searchPlaceholder: {
+    type: String,
+    default: 'Search',
+  },
+  title: {
+    type: String,
+    default: '',
+  },
+  hintIcon: {
+    type: String,
+    default: '',
+  },
+  hintText: {
+    type: String,
+    default: '',
+  },
+  validateFn: {
+    type: Function,
+    default: null,
+  },
+  generalErrorMessage: {
+    type: String,
+    default: 'Выберите компанию из списка',
+  },
+  generalIsError: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const searchQuery = ref('');
+const isOpen = ref(false);
+const selectedValue = ref(null);
+const showHint = ref(false);
+const isItemSelected = ref(false);
+const previousValue = ref('');
+const currentPlaceholder = ref(props.placeholder);
+const maxInputLength = 20;
+const generalErrorState = ref(props.generalIsError);
+const companyErrorState = ref(false);
+const generalErrorMessage = ref(props.generalErrorMessage);
+const companyErrorMessage = ref('');
+const isReadOnly = computed(() => {
+  // Check if there's a readOnly item to set the field as readonly
+  const readOnlyItem = props.items.find(item => item.readOnly);
+  if (readOnlyItem) {
+    searchQuery.value = readOnlyItem.name;
+    return true;
+  }
+  return false;
+});
+
+const filterItems = () => {
+  if (!isOpen.value && !isReadOnly.value) {
     isOpen.value = true;
-    currentPlaceholder.value = props.searchPlaceholder; // Изменяем плейсхолдер на "Search"
-  };
-  
-  const onBlur = () => {
-    nextTick(() => {
-      if (!isItemSelected.value) {
-        searchQuery.value = previousValue.value;
-        isOpen.value = false;
-        currentPlaceholder.value = props.placeholder; // Возвращаем первоначальный плейсхолдер
-      }
-    });
-  };
-  
-  const selectItem = (item) => {
-    selectedValue.value = item;
-    searchQuery.value = item;
-    isItemSelected.value = true;
-    isOpen.value = false;
-    currentPlaceholder.value = props.placeholder; // Возвращаем первоначальный плейсхолдер после выбора элемента
-  };
-  
-  const toggleDropdown = () => {
-    isOpen.value = !isOpen.value;
-  };
-  
-  const filteredItems = computed(() => {
-    return props.items.filter(item => item.toLowerCase().includes(searchQuery.value.toLowerCase()));
+  }
+};
+
+const onInput = () => {
+  if (isReadOnly.value) return; // Prevent further input when readOnly
+  filterItems();
+  validateInput(searchQuery.value);
+};
+
+const onFocus = () => {
+  if (isReadOnly.value) return; // Prevent focus when readOnly
+  previousValue.value = searchQuery.value;
+  if (isItemSelected.value) {
+    searchQuery.value = '';
+    isItemSelected.value = false;
+  }
+  isOpen.value = true;
+  currentPlaceholder.value = props.searchPlaceholder;
+};
+
+const onBlur = () => {
+  nextTick(() => {
+    if (!isItemSelected.value && !isReadOnly.value) {
+      searchQuery.value = previousValue.value;
+      isOpen.value = false;
+      currentPlaceholder.value = props.placeholder;
+    }
   });
-  </script>
-  
-  <style lang="scss">
-  .select-container {
-    position: relative;
-    width: 300px;
+};
+
+const selectItem = (item) => {
+  if (item.readOnly) return; // Prevent selection when readOnly
+
+  selectedValue.value = item.name;
+  searchQuery.value = item.name;
+  isItemSelected.value = true;
+  isOpen.value = false;
+  currentPlaceholder.value = props.placeholder;
+
+  if (item.isError) {
+    companyErrorState.value = true;
+    companyErrorMessage.value = item.errorMessage;
+  } else {
+    companyErrorState.value = false;
+    companyErrorMessage.value = '';
   }
-  
-  .select-input-wrapper {
-    position: relative;
-    display: flex;
-    flex-direction: column;
+  generalErrorState.value = false;
+};
+
+const toggleDropdown = () => {
+  if (isReadOnly.value) return; // Prevent toggling when readOnly
+  isOpen.value = !isOpen.value;
+};
+
+const filteredItems = computed(() => {
+  return props.items
+    .filter(item => item.name.toLowerCase().includes(searchQuery.value.toLowerCase()))
+    .filter(item => !item.readOnly); // Exclude readOnly items from dropdown
+});
+
+const validateInput = (input) => {
+  if (props.validateFn) {
+    const validationResult = props.validateFn(input, props.items);
+    generalErrorState.value = validationResult.isError;
+    generalErrorMessage.value = validationResult.errorMessage;
+  } else {
+    if (!input) {
+      generalErrorState.value = true;
+      generalErrorMessage.value = 'Выберите компанию из списка';
+    } else {
+      const hasMatch = props.items.some(item => item.name.toLowerCase().includes(input.toLowerCase()));
+      generalErrorState.value = !hasMatch;
+      generalErrorMessage.value = !hasMatch ? props.generalErrorMessage : '';
+    }
   }
-  
-  .select-title {
-    position: absolute;
-    left: 4px;
-    font-size: 0.75rem;
-    color: #666;
-    pointer-events: none;
-    transition: 0.2s;
-    font-weight: bold;
-    z-index: 1;
+
+  const matchingCompany = props.items.find(item => item.name.toLowerCase() === input.toLowerCase());
+  if (matchingCompany && matchingCompany.isError) {
+    companyErrorState.value = true;
+    companyErrorMessage.value = matchingCompany.errorMessage;
+  } else {
+    companyErrorState.value = false;
+    companyErrorMessage.value = '';
   }
-  
-  .input-wrapper {
-    position: relative;
-    display: flex;
-    align-items: center;
-  }
-  
-  .select-input {
-    width: 100%;
-    padding: 16px 46px 16px 4px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    box-sizing: border-box;
+};
+
+watch(() => props.generalIsError, (newVal) => {
+  generalErrorState.value = newVal;
+});
+</script>
+
+<style lang="scss">
+.select-container {
+  position: relative;
+  width: 300px;
+}
+
+.select-input-wrapper {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.select-title {
+  position: absolute;
+  left: 4px;
+  font-size: 0.75rem;
+  color: #666;
+  pointer-events: none;
+  transition: 0.2s;
+  font-weight: bold;
+  z-index: 1;
+}
+
+.input-wrapper {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+
+.select-input {
+  width: 100%;
+  padding: 16px 46px 16px 4px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  background-color: var(--input-bg, white);
+  cursor: var(--input-cursor, text);
+
+  &::placeholder {
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
-  
-    &::placeholder {
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
   }
-  
-  .select-input:focus + .select-title,
-  .select-input:not(:placeholder-shown) + .select-title {
-    top: -10px;
-    font-size: 0.65rem;
-    color: #007bff;
+
+  &:read-only {
+    background-color: #f0f0f0; /* Gray background for read-only */
+    cursor: not-allowed; /* Not-allowed cursor for read-only */
   }
-  
-  .hint-icon {
-    position: absolute;
-    right: 26px;
-    top: 50%;
-    transform: translateY(-50%);
-    cursor: pointer;
-  }
-  
-  .hint-text {
-    position: absolute;
-    top: -20px;
-    right: 45px;
-    background: #000;
-    color: #fff;
-    padding: 2px 8px;
-    border-radius: 4px;
-    font-size: 0.75rem;
-    white-space: nowrap;
-    z-index: 1;
-  }
-  
-  .icon {
-    position: absolute;
-    right: 10px;
-    top: 50%;
-    transform: translateY(-50%);
-    cursor: pointer;
-  }
-  
-  .icon.arrow-up::before {
-    content: '▲';
-  }
-  
-  .icon.arrow-down::before {
-    content: '▼';
-  }
-  </style>
-  
+}
+
+.select-input:focus + .select-title,
+.select-input:not(:placeholder-shown) + .select-title {
+  top: -10px;
+  font-size: 0.65rem;
+  color: #007bff;
+}
+
+.hint-icon {
+  position: absolute;
+  right: 26px;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+}
+
+.hint-text {
+  position: absolute;
+  bottom: -20px;
+  right: -60px;
+  background: #000;
+  color: #fff;
+  padding: 2px 8px;
+  border-radius: 4px;
+  font-size: 0.75rem;
+  white-space: nowrap;
+  z-index: 1;
+}
+
+.icon {
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+}
+
+.icon.arrow-up::before {
+  content: '▲';
+}
+
+.icon.arrow-down::before {
+  content: '▼';
+}
+
+.error-message {
+  color: red;
+  font-size: 0.75rem;
+  margin-top: 4px;
+}
+</style>
