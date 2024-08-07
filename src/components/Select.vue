3ea3ef7758a2716d920a7cf198b94ce73271ee82
Arsnyi <arsenyi.popov@gmail.com>
2024-08-07 11:44:58 +0300

    07.08 11:44 style onleRead

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -4,7 +4,7 @@
       <label 
         v-if="title"
         class="select-title"
-        :class="{ 'error-color': generalErrorState || companyErrorState }">
+        :class="{ 'error-color': generalErrorState || companyErrorState, 'read-only-color': isReadOnly }">
         {{ title }}</label>
       <div class="input-wrapper">
         <input
@@ -22,9 +22,9 @@
         />
         <span v-if="hintIcon" class="hint-icon" @mouseover="showHint = true" @mouseleave="showHint = false">
           <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" clip-rule="evenodd" d="M9 16.3632C13.2924 16.3632 16.3632 13.2924 16.3632 9C16.3632 4.7076 13.2924 1.6368 9 1.6368C4.7076 1.6368 1.6368 4.7076 1.6368 9C1.6368 13.2924 4.7076 16.3632 9 16.3632ZM9 18C6.61305 18 4.32387 17.0518 2.63604 15.364C0.948212 13.6761 0 11.3869 0 9C0 6.61305 0.948212 4.32387 2.63604 2.63604C4.32387 0.948212 6.61305 0 9 0C11.3869 0 13.6761 0.948212 15.364 2.63604C17.0518 4.32387 18 6.61305 18 9C18 11.3869 17.0518 13.6761 15.364 15.364C13.6761 17.0518 11.3869 18 9 18Z" fill="#828D9B"/>
-  <path d="M8.08967 14V7.45455H9.90501V14H8.08967ZM9.0016 6.1108C8.73171 6.1108 8.50018 6.02131 8.307 5.84233C8.11665 5.66051 8.02148 5.44318 8.02148 5.19034C8.02148 4.94034 8.11665 4.72585 8.307 4.54688C8.50018 4.36506 8.73171 4.27415 9.0016 4.27415C9.27148 4.27415 9.5016 4.36506 9.69194 4.54688C9.88512 4.72585 9.98171 4.94034 9.98171 5.19034C9.98171 5.44318 9.88512 5.66051 9.69194 5.84233C9.5016 6.02131 9.27148 6.1108 9.0016 6.1108Z" fill="#828D9B"/>
-</svg>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M9 16.3632C13.2924 16.3632 16.3632 13.2924 16.3632 9C16.3632 4.7076 13.2924 1.6368 9 1.6368C4.7076 1.6368 1.6368 4.7076 1.6368 9C1.6368 13.2924 4.7076 16.3632 9 16.3632ZM9 18C6.61305 18 4.32387 17.0518 2.63604 15.364C0.948212 13.6761 0 11.3869 0 9C0 6.61305 0.948212 4.32387 2.63604 2.63604C4.32387 0.948212 6.61305 0 9 0C11.3869 0 13.6761 0.948212 15.364 2.63604C17.0518 4.32387 18 6.61305 18 9C18 11.3869 17.0518 13.6761 15.364 15.364C13.6761 17.0518 11.3869 18 9 18Z" fill="#828D9B"/>
+            <path d="M8.08967 14V7.45455H9.90501V14H8.08967ZM9.0016 6.1108C8.73171 6.1108 8.50018 6.02131 8.307 5.84233C8.11665 5.66051 8.02148 5.44318 8.02148 5.19034C8.02148 4.94034 8.11665 4.72585 8.307 4.54688C8.50018 4.36506 8.73171 4.27415 9.0016 4.27415C9.27148 4.27415 9.5016 4.36506 9.69194 4.54688C9.88512 4.72585 9.98171 4.94034 9.98171 5.19034C9.98171 5.44318 9.88512 5.66051 9.69194 5.84233C9.5016 6.02131 9.27148 6.1108 9.0016 6.1108Z" fill="#828D9B"/>
+          </svg>
         </span>
         <span v-if="showHint" class="hint-text">{{ hintText }}</span>
       </div>
@@ -35,11 +35,11 @@
         @click="toggleDropdown"
       >
       <svg v-if="!isOpen" width="13" height="10" viewBox="0 0 13 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M12 3L6.5 8L1 3" stroke="#292929" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-<svg v-else width="13" height="10" viewBox="0 0 13 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M1 7L6.5 2L12 7" stroke="#292929" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
+        <path d="M12 3L6.5 8L1 3" stroke="#292929" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+      <svg v-else width="13" height="10" viewBox="0 0 13 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 7L6.5 2L12 7" stroke="#292929" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
       </span>
     </div>
     <p v-if="generalErrorState" class="error-message">{{ generalErrorMessage }}</p>
@@ -47,6 +47,7 @@
     <Dropdown v-if="isOpen && !isReadOnly && filteredItems.length" :items="filteredItems" @select="selectItem" />
   </div>
 </template>
+
 
 
 <script setup>
@@ -209,6 +210,9 @@ watch(() => props.generalIsError, (newVal) => {
   z-index: 1;
   color: #292929;
 }
+.select-title.read-only-color {
+  color: #bfc7d3;
+}
 
 .input-wrapper {
   position: relative;
@@ -238,7 +242,7 @@ watch(() => props.generalIsError, (newVal) => {
   }
 
   &:read-only {
-    background-color: #f0f0f0; /* Gray background for read-only */
+    background-color: #f5f7fb; /* Gray background for read-only */
     cursor: not-allowed; /* Not-allowed cursor for read-only */
   }
 }
